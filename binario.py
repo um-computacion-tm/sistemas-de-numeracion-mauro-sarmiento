29c81e4ca19439d14b28634f2b51c81a994b34d0
Mauro Sarmiento <mn.sarmiento@alumno.um.edu.ar>
2023-05-01 12:10:15 -0300

    Terminado de binario al resto de sistemas

--- binario.py
+++ binario.py
@@ -1,10 +1,46 @@
 
 class Binario:
     def binarioADecimal(self, numero_binario):
-        pass
+        numero_binario = str(numero_binario)
+        decimal_list = []
+        decimal = 0
+        base = len(numero_binario) - 1
+        for k in range(len(numero_binario)):
+            base_num = 2**base
+            decimal_list.append(base_num)
+            base -= 1
+        for i in range(len(decimal_list)):
+            value = decimal_list[i] * int(numero_binario[i])
+            decimal += value
+        return decimal
+
 
     def binarioAHexadecimal(self, numero_binario):
-        pass
+        numero_binario = str(numero_binario)
+        for k in range(len(numero_binario)):
+            if (len(numero_binario) % 4) != 0:
+                numero_binario = '0' + numero_binario
+        binario = {'0000': 0, '0001': 1, '0010': 2, '0011': 3, '0100': 4, '0101': 5, '0110': 6, '0111': 7, '1000': 8
+                , '1001': 9, '1010': 'A', '1011': 'B', '1100': 'C', '1101': 'D', '1110': 'E', '1111': 'F'}
+        hexadecimal = ''
+        for i in range(0, len(numero_binario), 4):
+            division = numero_binario[i:i+4]
+            if division in binario:
+                hexadecimal += str(binario[division])
+        return hexadecimal
+
 
     def binarioAOctal(self, numero_binario):
-        pass
+        numero_binario = str(numero_binario)
+        for k in range(len(numero_binario)):
+            if (len(numero_binario) % 3) != 0:
+                numero_binario = '0' + numero_binario
+        binario = {'000': 0, '001': 1, '010': 2, '011': 3, '100': 4, '101': 5, '110': 6, '111': 7}
+        octal = ''
+        for i in range(0, len(numero_binario), 3):
+            division = numero_binario[i:i+3]
+            if division in binario:
+                octal += str(binario[division])
+        return int(octal)
+
+
--- test_sistemas.py
+++ test_sistemas.py
@@ -4,7 +4,61 @@ from octal import Octal
 from binario import Binario
 from decimal import Decimal
 
-class TestSistemasHexadecimal(unittest.TestCase):
+class TestSistemaBinario(unittest.TestCase):
 
-    def testHexadecimalABinario(self):
-        pass
+    #! BINARIO A DECIMAL
+
+    def testBinarioADecimal(self):
+        bin = Binario()
+        result = bin.binarioADecimal('1010011')
+        self.assertEqual(result, 83)
+
+    def testBinarioADecimal_II(self):
+        bin = Binario()
+        result = bin.binarioADecimal('10110')
+        self.assertEqual(result, 22)
+
+    def testBinarioADecimal_III(self):
+        bin = Binario()
+        result = bin.binarioADecimal('11010011010')
+        self.assertEqual(result, 1690)
+    
+    #! BINARIO A HEXADECIMAL
+
+    def testBinarioAHexadecimal(self):
+        bin = Binario()
+        result = bin.binarioAHexadecimal('11110111')
+        self.assertEqual(result, 'F7')
+
+    def testBinarioAHexadecimal_II(self):
+        bin = Binario()
+        result = bin.binarioAHexadecimal('100000000100')
+        self.assertEqual(result, '804')
+
+    def testBinarioAHexadecimal_III(self):
+        bin = Binario()
+        result = bin.binarioAHexadecimal('10110010111')
+        self.assertEqual(result, '597')
+    
+    #! BINARIO A OCTAL
+
+    def testBinarioAOctal(self):
+        bin = Binario()
+        result = bin.binarioAOctal('110010')
+        self.assertEqual(result, 62)
+
+    def testBinarioAOctal_II(self):
+        bin = Binario()
+        result = bin.binarioAOctal('111101111')
+        self.assertEqual(result, 757)
+
+    def testBinarioAOctal_III(self):
+        bin = Binario()
+        result = bin.binarioAOctal('10011011')
+        self.assertEqual(result, 233)
+
+    
+
+
+if __name__ == '__main__':
+    unittest.main()
